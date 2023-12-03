e4dd1e986350fbf880201387e8ac0ebe1393cb4d
Vladimir Luzin <vvluzin2004@gmail.com>
2023-12-04 03:00:43 +0500

    public read from all pages

--- crud/router.py
+++ crud/router.py
@@ -12,8 +12,37 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 
 
+@router.get("/members", response_class=HTMLResponse)
+def get_base_page(request: Request):
+    cursor.execute("SELECT * FROM members;")
+    data = cursor.fetchall()
+    return templates.TemplateResponse("members.html", {"request": request, "data": data})
+
+
 @router.get("/trainers", response_class=HTMLResponse)
 def get_base_page(request: Request):
-    cursor.execute("SELECT * FROM trainers;")
+    cursor.execute(
+        "SELECT trainers.id,trainers.surname,trainers.name,trainers.patronymic,trainers.birth_date,ranks.name,ranks.payment FROM trainers INNER JOIN ranks ON trainers.rank_id = ranks.id")
     data = cursor.fetchall()
     return templates.TemplateResponse("trainers.html", {"request": request, "data": data})
+
+
+@router.get("/classes", response_class=HTMLResponse)
+def get_base_page(request: Request):
+    cursor.execute("SELECT * FROM classes;")
+    data = cursor.fetchall()
+    return templates.TemplateResponse("classes.html", {"request": request, "data": data})
+
+
+@router.get("/events", response_class=HTMLResponse)
+def get_base_page(request: Request):
+    cursor.execute("SELECT * FROM events;")
+    data = cursor.fetchall()
+    return templates.TemplateResponse("events.html", {"request": request, "data": data})
+
+
+@router.get("/records", response_class=HTMLResponse)
+def get_base_page(request: Request):
+    cursor.execute("SELECT * FROM records;")
+    data = cursor.fetchall()
+    return templates.TemplateResponse("records.html", {"request": request, "data": data})
