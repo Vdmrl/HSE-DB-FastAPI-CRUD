a30f0c274551da26b3c06a92abc36ab59e675ec9
Vladimir Luzin <vvluzin2004@gmail.com>
2023-12-06 01:44:32 +0500

    config with .env

--- database/database.py
+++ database/database.py
@@ -1,13 +1,13 @@
 import psycopg2
 from psycopg2.extras import DictCursor
 from time import sleep
+import config as conf
 
 # connection to databse
 cursor=None
 while not cursor:
     try:
-        print("password: ", end="")
-        conn = psycopg2.connect(host="localhost", database="hse_db_course", user="postgres", password=input(),
+        conn = psycopg2.connect(host=conf.DB_HOST, database=conf.DB_NAME, user=conf.DB_USER, password=conf.DB_PASS,
                                 cursor_factory=DictCursor)
         cursor = conn.cursor()
         print("database successful connection")
